16d2a33ce0a0b3185035a52e5afeadcfc96a73e8
Rian Lucas <rianluccas08@gmail.com>
2024-01-05 16:25:54 -0300

    feat: Create Permission model and migration

---
+++ app/Models/Permission.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Permission extends Model
+{
+    use HasFactory, HasUuids;
+
+    public function users(): BelongsToMany {
+        $this->belongsToMany(User::class);
+    }
+}
